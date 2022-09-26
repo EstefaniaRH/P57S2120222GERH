1987ba8db3957b52536f799e38cae33884c55649
Estefania Rodriguez <estefan.rh03@gmail.com>
2022-09-25 21:04:43 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/g7s21/InicialActivity.java
@@ -0,0 +1,24 @@
+package mx.edu.tesoem.isc.g7s21;
+
+import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class InicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(InicialActivity.this, PrincipalActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+    }
+}
